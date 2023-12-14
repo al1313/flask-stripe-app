916711560b10376a88d280ab8517ac89816d5f2c
Andy Lai <andylai13@gmail.com>
2023-12-14 13:46:38 +1100

    Enable Stripe check out

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import stripe
 
-# local files
-# from config import stripe_keys
-from helpers import success_response, error_response, check_json
-
-
 app = Flask(__name__, instance_relative_config=True)
+
+# more secured way to load configs that are sensitive
+# https://flask.palletsprojects.com/en/3.0.x/config/#instance-folders
 app.config.from_pyfile("testing.py")
 
 
@@ -17,72 +15,18 @@ def home_page():
     )
 
 
-@app.route("/confirmation")
-def confirmation_page():
-    id = request.args.get("id", default="", type=str)
-    amount = request.args.get("amount", default="", type=str)
-    email = request.args.get("email", default="", type=str)
-    product = request.args.get("product", default="", type=str)
-
-    return render_template(
-        "confirmation.html", id=id, email=email, product=product, amount=amount
-    )
-
-
-@app.route("/api/charge", methods=["POST"])
-def charge_api():
-    if request.method == "POST":
-        # grab json from request body
-        json = request.json
-        # check if all keys present, return error if any missing
-        result = check_json(json, ["email", "stripe_token", "description", "amount"])
-        if result:
-            return error_response("missing {}".format(result))
-        # add stripe api key
-        stripe.api_key = app.config["STRIPE_SECRET_KEY"]
-        try:
-            # create customer ID
-            customer = stripe.Customer.create(
-                email=json["email"], source=json["stripe_token"]
-            )
-            # create charge
-            charge = stripe.Charge.create(
-                customer=customer.id,
-                amount=json["amount"],
-                currency="usd",
-                description=json["description"],
-            )
-            # jsonify'd 200 success
-            return success_response(charge)
-
-        except stripe.error.StripeError as e:
-            # jsonify'd 500 error
-            return error_response(e)
-
-
-YOUR_DOMAIN = "http://localhost:5008"
-
-
 @app.route("/create-checkout-session", methods=["POST"])
 def create_checkout_session():
     stripe.api_key = app.config["STRIPE_SECRET_KEY"]
     try:
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    "price": "price_1OMfgeJopRORFg9GWw8uMzD5",
-                    "quantity": 1,
-                },
-                {
-                    "price": "price_1OMfgDJopRORFg9GNPTxl4DR",
-                    "quantity": 1,
-                },
-            ],
+            customer_creation="always",
+            line_items=request.json,
+            # Creating custom fields in check out page
             custom_fields=[
                 {
-                    "key": "childcare_location",
-                    "label": {"type": "custom", "custom": "Centre Location"},
+                    "key": "class_location",
+                    "label": {"type": "custom", "custom": "Class Location"},
                     "type": "text",
                 },
                 {
@@ -98,18 +42,22 @@ def create_checkout_session():
                 },
             ],
             mode="payment",
-            success_url=YOUR_DOMAIN + "/success",
-            cancel_url=YOUR_DOMAIN + "/cancel",
+            # Pass check out session ID to success URL so we can extract information to render as HTML
+            success_url=request.host_url + "success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=request.host_url + "cancel",
         )
     except Exception as e:
-        return str(e)
+        return jsonify(error=str(e)), 403
 
-    return redirect(checkout_session.url, code=303)
+    return jsonify({"sessionId": checkout_session["id"]})
 
 
 @app.route("/success")
 def success():
-    return render_template("success.html")
+    session_id = request.args.get("session_id", default="", type=str)
+    session = stripe.checkout.Session.retrieve(session_id)
+    customer = stripe.Customer.retrieve(session.customer)
+    return render_template("success.html", customer_name=customer.name)
 
 
 @app.route("/cancel")
--- static/index.js
+++ static/index.js
@@ -4,8 +4,8 @@ function initAddProductButton() {
 
         var elem = $(this);
 
-        // get price_id
-        var price_id = elem.attr('data-price-id');
+        // get priceId - each product and price combination has a unique price id in Stripe
+        var priceId = elem.attr('data-price-id');
 
         // add to localstorage
         var obj = {
@@ -14,24 +14,24 @@ function initAddProductButton() {
             "imgSrc": elem.closest('.thumbnail').children('img').attr('src'),
             "qty": 1,
         }
-        saveToLocal(price_id, obj);
-
-        var my_cart = JSON.parse(localStorage.getItem('cart'));
-
+        saveToLocal(priceId, obj);
+      
+        // Generate html for cart
+        var myCart = JSON.parse(localStorage.getItem('cart'));        
         $('.dropdown-cart').html()
 
         var html = '';
 
-        for (var key in my_cart) {
+        for (var key in myCart) {
             // add to cart html
             html = html.concat(`
                     <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">                    
-                            <img src="${my_cart[key]['imgSrc']}" alt="" width="50rem" />
+                            <img src="${myCart[key]['imgSrc']}" alt="" width="50rem" />
                             <div class="d-flex w-100 justify-content-between">
                                 <div class="row">
-                                    <span>${my_cart[key]['product_name']}</span>
-                                    <span>Price: ${my_cart[key]['price']}</span>
-                                    <span>Quantity: ${my_cart[key]['qty']}</span>    
+                                    <span>${myCart[key]['product_name']}</span>
+                                    <span>Price: ${myCart[key]['price']}</span>
+                                    <span>Quantity: ${myCart[key]['qty']}</span>    
                                 </div>                                
                             </div>                        
                     </li>
@@ -44,23 +44,22 @@ function initAddProductButton() {
         var currentCount = parseInt($('#cart-count').html());
         $('#cart-count').html(currentCount += 1);
 
-
     })
 }
 
-function saveToLocal(price_id, obj) {
+function saveToLocal(priceId, obj) {
     // https://stackoverflow.com/questions/16083919/push-json-objects-to-array-in-localstorage
-    // var a = {};
     // Parse the serialized data back into an aray of objects
-    var a = JSON.parse(localStorage.getItem('cart'));
-    // Push the new data (whether it be an object or anything else) onto the array
-    if (!a.hasOwnProperty(price_id)) {
-        a[price_id] = obj;
+    var cart = JSON.parse(localStorage.getItem('cart'));
+    // If price ID not in cart, create the key in the cart object
+    // Otherwise increment the quantity by 1
+    if (!cart.hasOwnProperty(priceId)) {
+        cart[priceId] = obj;
     } else {
-        a[price_id]['qty'] += 1;
+        cart[priceId]['qty'] += 1;
     };
     // Re-serialize the array back into a string and store it in localStorage
-    localStorage.setItem('cart', JSON.stringify(a));
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 function initClearCart() {
@@ -79,90 +78,49 @@ function clearCart() {
 function initMyCheckoutButton() {
     $('#mycheckout').click(function (e) {
         e.preventDefault();
-        var data = {
-            "email": "test.com",
-        }
-        const stripe = Stripe($('#stripeKey').attr("data-key"));
         
-        fetch("/create-checkout-session")
-            .then((result) => { return result.json(); })
+        // Get cart data from local storage, then build list required by Stripe Checkout
+        var cart = JSON.parse(localStorage.getItem('cart'));
+        var cartDataForStripe = []
+
+        for (var key in cart) {
+            cartDataForStripe.push(
+                {
+                    "price": key,
+                    "quantity": cart[key]['qty']
+                }
+            )
+        }
+
+        const stripe = Stripe($('#stripeKey').attr("data-key"));
+
+        // POST request to Flask backend that will create the Checkout session
+        fetch("/create-checkout-session", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(cartDataForStripe)
+        })
+            .then((response) => { return response.json(); })
             .then((data) => {
                 console.log(data);
                 // Redirect to Stripe Checkout
                 return stripe.redirectToCheckout({ sessionId: data.sessionId })
             })
-            .then((res) => {
-                console.log(res);
+            .then(function (result) {
+                console.log(result);
+                if (result.error) {
+                    alert(result.error.message);
+                }
             });
 
     })
 }
-
-
-function initCheckoutButton() {
-    $('#checkout-cart').click(function (e) {
-        e.preventDefault();
-        //
-        var productStr = '';
-        var totalAmount = 0;
-        // build product string
-        var cart = JSON.parse(localStorage.getItem('cart'));
-        // build vars for cart modal
-        cart.forEach(function (item, index) {
-            totalAmount += item.amount;
-            productStr += item.product;
-            // last one
-            if (index !== cart.length - 1) {
-                productStr += " and ";
-            }
-        });
-        // open cart modal
-        var handler = stripeHandlder(totalAmount, productStr);
-        handler.open({
-            name: productStr,
-            description: productStr,
-            amount: totalAmount
-        });
-    })
-}
-
-function stripeHandlder(totalAmount, productStr) {
-    var handler = StripeCheckout.configure({
-        key: $('#stripeKey').attr("data-key"),
-        locale: "auto",
-        token: function (token) {
-
-            var data = {
-                "email": token.email,
-                "stripe_token": token.id,
-                "description": productStr,
-                "amount": totalAmount
-            }
-
-            $.ajax({
-                url: "/api/charge",
-                method: "POST",
-                contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(data),
-            }).done(function (response) {
-                var r = response.response;
-                // redirect
-                var url = `/confirmation?amount=${(r.amount / 100).toFixed(2)}&id=${r.id}&email=${r.billing_details.name}&product=${r.description}`
-                window.location.href = url;
-            }).fail(function (response) {
-                console.log(response)
-            });
-        }
-    });
-    return handler
-}
-
-
 
 $(document).ready(function () {
     clearCart();
     initAddProductButton();
     initClearCart();
-    initCheckoutButton();
     initMyCheckoutButton();
 });
